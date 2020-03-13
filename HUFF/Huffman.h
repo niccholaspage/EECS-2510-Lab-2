bdf21c883cb7ff04093ca21c2537d89a63c0d38a
niccholaspage <niccholaspage@gmail.com>
2020-03-13 16:51:11 -0400

    Finish header comments

--- HUFF/Huffman.h
+++ HUFF/Huffman.h
@@ -12,11 +12,11 @@ public:
 	Huffman();
 	~Huffman();
 
-	void MakeTreeBuilder(string inputFile, string outputFile);
-	void EncodeFile(string inputFile, string outputFile);
-	void DecodeFile(string inputFile, string outputFile);
-	void EncodeFileWithTree(string inputFile, string treeFile, string outputFile);
-	void DisplayHelp();
+	void MakeTreeBuilder(string inputFile, string outputFile);	// Makes a tree builder file from the given input file in the specified output file
+	void EncodeFile(string inputFile, string outputFile);		// Encodes the given input file into the given output file
+	void DecodeFile(string inputFile, string outputFile);		// Decodes the given input file into the given output file
+	void EncodeFileWithTree(string inputFile, string treeFile, string outputFile); // Encodes the given input file, using the given tree builder file, into the given output file
+	void DisplayHelp(); // Displays information on how to use the program
 private:
 	struct treenode {
 		unsigned char symbol = NULL;	// The symbol of the node
@@ -42,13 +42,13 @@ private:
 	int getIndexOfSmallestNode(int skipIndex); // Returns the smallest node index in the array, skipping the given index
 	void buildTree(); // Builds the tree of nodes by reading the input file and determining frequencies and writes the combinations of nodes to the output stream
 	void buildTreeFromTreeBuilder(ifstream& stream, bool writeToOutput); // Builds the tree of nodes by combining nodes based on the given stream.
-	void buildEncodingTable();
-	void buildEncodingTable(treenode* node, string currentPath);
-	void decodeBytes();
-	void encodeBits(unsigned char& outputCharacter, int& currentBit, string& bits);
-	void encodeBytes();
-	void navigateTree(unsigned char byte, int bitToCheck, treenode*& node);
-	void printFinalInfo(string& input_file_path, string& output_file_path);
-	string formatUnsignedInt(unsigned int number);
-	bool isLeaf(treenode* node);
+	void buildEncodingTable(); // Builds the encoding table, which is used to encode each character in a file
+	void buildEncodingTable(treenode* node, string currentPath); // Recursively builds encoding table by starting at the given node and traversing through its children
+	void decodeBytes(); // Decodes the bytes of the input file
+	void encodeBits(unsigned char& outputCharacter, int& currentBit, string& bits); // Encodes the given bits into the output file
+	void encodeBytes(); // Encodes the bytes of the input file
+	void navigateTree(unsigned char byte, int bitToCheck, treenode*& node); // Navigates through the tree by checking the given bit and navigating to the left and right child of the given node
+	void printFinalInfo(string& input_file_path, string& output_file_path); // Prints the final information after the operation ran, like the time elapsed and bytes in and out
+	string formatUnsignedInt(unsigned int number); // Formats an unsigned integer by inserting commas into it, returning a string
+	bool isLeaf(treenode* node); // Checks if the given node is a leaf
 };
