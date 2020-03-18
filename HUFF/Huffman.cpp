562af5f3b3128089c4f3db39f9e9d02e4694c128
niccholaspage <niccholaspage@gmail.com>
2020-03-17 22:00:54 -0400

    Fix dash of display help

--- HUFF/Huffman.cpp
+++ HUFF/Huffman.cpp
@@ -660,5 +660,5 @@ void Huffman::DisplayHelp()
 	cout << "-e file1 [file2] - Encodes file1, placing the encrypted version into file2. If file2 is not specified, file2 will have the same name as file1, minus the extension, which will be .huf.\n";
 	cout << "-d file1 file 2 - Decodes file1, placing the decrypted version into file1.\n";
 	cout << "-t file1 [file2] - Creates a 510 byte tree-builder file for file1, and places it into file2.\n";
-	cout << "–et file1 file2 [file3] - Encodes file1 with the tree built from file2 and places it into file3. If file3 is not specified, the output file will have the same name as file1 with the .huf extension.\n";
+	cout << "-et file1 file2 [file3] - Encodes file1 with the tree built from file2 and places it into file3. If file3 is not specified, the output file will have the same name as file1 with the .huf extension.\n";
 }
